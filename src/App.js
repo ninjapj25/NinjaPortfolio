1f404506f997e32cd46e248c8fd0a979df464730
reybaliguat <rey@ajio.co.jp>
2020-12-07 19:40:50 +0800

    final change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Grid container={true}>
         <SideNavigation />
         <Switch>
-          <Route exact path="/home" component={Home} />
+          <Route path="/home" component={Home} />
           <Route path="/NinjaPortfolio" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/works" component={Works} />
